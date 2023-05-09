8b7ec7e0d7bf4143eda4f2431837eabe2c07be4b
Jonas Arden <itsjonasarden@gmail.com>
2023-05-09 12:05:25 +0200

    chore: add safe area view

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { StyleSheet, View } from "react-native";
+import { SafeAreaView, Platform, StatusBar } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return <SafeAreaView style={{ flex: 1, marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0 }}></SafeAreaView>;
 }
-
-const styles = StyleSheet.create({});
